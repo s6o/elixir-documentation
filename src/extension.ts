f57762b5a872c50471c5d13efcdc13574fd31a10
Oliver Sõro <oliver.s6ro@gmail.com>
2023-11-07 22:05:13 +0200

    Refactor URI construction; module, function selection via QuickPick.
    Get LSP completion options via vscode.executeCompletionItemProvider.
    
    Now minimal usable version.

--- src/extension.ts
+++ src/extension.ts
@@ -5,19 +5,54 @@ import util from 'util';
 import { execFile as execNonPromise } from 'child_process';
 const execFile = util.promisify(execNonPromise);
 
-async function elixirOTPVersion(): Promise<[string, string]> {
+type DocRef = {
+  erlBase: string;
+  hexBase: string;
+  elixirVersion: string;
+  module: string;
+  fragment: string;
+  isErl: boolean;
+};
+
+async function initDocRef(): Promise<DocRef> {
+  const ref = {
+    erlBase: 'https://www.erlang.org/doc/man',
+    hexBase: 'https://hexdocs.pm', // TODO: get from configuration
+    elixirVersion: '',
+    module: 'Kernel',
+    fragment: '',
+    isErl: false,
+  };
+
   const { stdout, stderr } = await execFile('iex', ['-v']);
   if (stdout) {
     console.log('stdout:', stdout);
-
     const results = [...stdout.matchAll(/IEx\s+([\d\.]+).+OTP\s+(\d+)/g)];
     if (results && results[0]) {
+      ref.elixirVersion = results[0][1];
       console.log(`Elixir ${results[0][1]}`);
       console.log(`OTP ${results[0][2]}`);
-      return [results[0][1], results[0][2]];
     }
   }
-  return ['', ''];
+  return ref;
+}
+
+function toDocUrl(ref: DocRef): string {
+  if (ref.isErl) {
+    return `${ref.erlBase}/${ref.module}${
+      ref.fragment ? `#${ref.fragment}` : ''
+    }`;
+  } else {
+    return `${ref.hexBase}/elixir/${ref.elixirVersion}/${ref.module}.html${
+      ref.fragment ? `#${ref.fragment}` : ''
+    }`;
+  }
+}
+
+function toMainDocUrl(ref: DocRef): string {
+  return `https://elixir-lang.org/docs.html#${majorElixirVersion(
+    ref.elixirVersion
+  )}`;
 }
 
 function majorElixirVersion(version: string): string {
@@ -29,6 +64,29 @@ function majorElixirVersion(version: string): string {
   }
 }
 
+function updateDocRef(ref: DocRef, item: vscode.QuickPickItem) {
+  const isFunction: boolean = item.detail?.startsWith('(function)') || false;
+  const isModule: boolean = item.detail !== null && item.detail === 'module';
+
+  if (isFunction) {
+    const startIndex = item.detail!.indexOf(' ') + 1;
+    const finalIndex = item.detail!.indexOf('.', startIndex);
+    const module = item.detail!.slice(startIndex, finalIndex);
+    ref.isErl = module.startsWith(':');
+    ref.module = ref.isErl ? module.slice(1) : module;
+    ref.fragment = ref.isErl ? item.label.replace('/', '-') : item.label;
+    return ref;
+  }
+
+  if (isModule) {
+    ref.isErl = item.label.startsWith(':');
+    ref.module = ref.isErl ? item.label.slice(1) : item.label;
+    return ref;
+  }
+
+  return ref;
+}
+
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
 export async function activate(context: vscode.ExtensionContext) {
@@ -38,7 +96,7 @@ export async function activate(context: vscode.ExtensionContext) {
     'Congratulations, your extension "Elixir Documentation Lookup" is now active!'
   );
 
-  const [elixirVersion, otpVersion] = await elixirOTPVersion();
+  let docRef = await initDocRef();
 
   // The command has been defined in the package.json file
   // Now provide the implementation of the command with registerCommand
@@ -51,14 +109,14 @@ export async function activate(context: vscode.ExtensionContext) {
       vscode.commands.executeCommand('workbench.action.focusSecondEditorGroup');
       vscode.commands.executeCommand(
         'simpleBrowser.api.open',
-        `https://elixir-lang.org/docs.html#${majorElixirVersion(elixirVersion)}`
+        toMainDocUrl(docRef)
       );
     }
   );
 
   let cmdLookup = vscode.commands.registerCommand(
     'elixir-documentation.lookup',
-    () => {
+    async () => {
       // Find text on current line
       const te = vscode.window.activeTextEditor;
       if (te) {
@@ -72,6 +130,31 @@ export async function activate(context: vscode.ExtensionContext) {
         if (range) {
           const word = te.document.getText(range);
           console.log(`Word on the line: ${word}`);
+
+          const completes: { items: Array<vscode.QuickPickItem> } =
+            await vscode.commands.executeCommand(
+              'vscode.executeCompletionItemProvider',
+              te.document.uri,
+              range.end
+            );
+          console.log(completes);
+          const result = await vscode.window.showQuickPick(
+            [
+              ...completes.items.map((e) => ({
+                label: e.label,
+                description: undefined,
+                detail: e.detail,
+              })),
+            ],
+            {
+              canPickMany: false,
+            }
+          );
+          if (result) {
+            console.log('QuickPick result:\n');
+            console.log(result);
+            docRef = updateDocRef(docRef, result);
+          }
         }
       }
       // The code you place here will be executed every time your command is executed
@@ -79,7 +162,7 @@ export async function activate(context: vscode.ExtensionContext) {
       vscode.commands.executeCommand('workbench.action.focusSecondEditorGroup');
       vscode.commands.executeCommand(
         'simpleBrowser.api.open',
-        `https://hexdocs.pm/elixir/${elixirVersion}/Kernel.html`
+        toDocUrl(docRef)
       );
     }
   );
