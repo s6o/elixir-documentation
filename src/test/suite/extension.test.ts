5bbf537ffc6edd660d5de123fc643fde154f01da
Oliver Sõro <oliver.s6ro@gmail.com>
2023-11-20 16:30:49 +0200

    First very basic test.

--- src/test/suite/extension.test.ts
+++ src/test/suite/extension.test.ts
@@ -3,13 +3,26 @@ import * as assert from 'assert';
 // You can import and use all API from the 'vscode' module
 // as well as import your extension to test it
 import * as vscode from 'vscode';
-// import * as myExtension from '../../extension';
+//import * as ext from '../../extension';
 
-suite('Extension Test Suite', () => {
-	vscode.window.showInformationMessage('Start all tests.');
+suite('Line parser regexp tests', () => {
+  vscode.window.showInformationMessage(
+    'Elixir documentation - line parsers regexp'
+  );
 
-	test('Sample test', () => {
-		assert.strictEqual(-1, [1, 2, 3].indexOf(5));
-		assert.strictEqual(-1, [1, 2, 3].indexOf(0));
-	});
+  test('Test alias|@behaviour|import|require|use Module(s)', () => {
+    const module =
+      /(?!alias|@behaviour|import|require|use)(?!\s+)[A-Z][\w\.]*/gmu;
+
+    const input =
+      'alias Postgrex.Connection dladfj require Logger dlad @behaviour Mailer dldl use GenServer';
+    const results = input.match(module);
+    console.log(results);
+
+    assert.strictEqual(results!.length, 4);
+    assert.strictEqual(results![0], 'Postgrex.Connection');
+    assert.strictEqual(results![1], 'Logger');
+    assert.strictEqual(results![2], 'Mailer');
+    assert.strictEqual(results![3], 'GenServer');
+  });
 });
